2af3615da17f1a517f3de6f37a3ace19284c8927
Korey Pace <korey.pace@gmail.com>
2015-05-05 06:15:51 -0400

    added ui and pages

--- angular/app/app.js
+++ angular/app/app.js
@@ -1,12 +1,32 @@
-var callListApp = angular.module("callListApp", ["ngRoute"]);
+var callListApp = angular.module("callListApp", ["ngRoute", "firebase"])
+.constant('FIREBASEURL', 'https://calltable.firebaseio.com/');
+
+
+
 
 callListApp.config(function($routeProvider){
 	$routeProvider
-	.when("/calls", {
-		templateUrl: "calllist.html",
+	.when("/login", {
+		templateUrl: "partials/login.html",
 		controller: "CallListController"
-	})
-	.otherwise ({ 
-	redirectTo: "/calls"
+	}).
+	when("/daily", {
+		templateUrl: "partials/daily.html",
+		controller: "DailyListController"
+	}).
+	when("/monthy", {
+		templateUrl: "partials/monthly.hmtl",
+		controller: "MonthlyListController"
+	}).
+	when("/weekly", {
+		templateUrl: "partials/weekly.html",
+		controller: "WeeklyListController"
+	}).
+	when("/callentry", {
+		templateUrl: "partials/callentry.html",
+		controller: "CallListController"
+	}).
+	otherwise ({ 
+	redirectTo: "/login"
 	});
 });
